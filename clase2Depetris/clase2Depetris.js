2986d7012d6d00d28274969dd9f45ed4e8ab2e74
Pablo Ignacio Depetris Chauvin <pdepetrischauvin@gmail.com>
2021-10-30 17:19:57 -0300

    Incorporación index y código faltante

--- clase2Depetris/clase2Depetris.js
+++ clase2Depetris/clase2Depetris.js
@@ -4,7 +4,7 @@ let year = 1989;
 if (nombre) {
   edad = prompt("Hola, " + nombre + ", ¿cuál es tu edad?");
   if (parseInt(edad) + year >= 2021) {
-    alert("Naciste en los 80s. Bienvenido al club.");
+    alert("Naciste en los 80s (e incluso más allá). Bienvenido al club.");
   } else {
     alert(
       "Sos bastante joven. Capaz no sepas que Nacha Guevara sacó un disco de heavy metal con tango allá por el '91."
